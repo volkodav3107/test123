c53a1f4e9d0746467cd8bf891b26477b219be484
Vladislav Boyko <for.work.vboyko@yandex.ru>
2018-03-14 20:38:21 +0700

    add UI

--- app/src/main/java/ru/gooddocs/vladislav/myapplication/MainActivity.java
+++ app/src/main/java/ru/gooddocs/vladislav/myapplication/MainActivity.java
@@ -2,15 +2,28 @@ package ru.gooddocs.vladislav.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Cool activity
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Test";
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+            }
+        });
     }
 }
